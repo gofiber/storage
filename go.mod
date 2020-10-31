ad49a4d6a625eea1c3545a7db5c897fa046718ee
Fenny <25108519+Fenny@users.noreply.github.com>
2020-10-31 06:20:25 +0100

    👷‍♂️ add skeletons

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gofiber/storage
+
+go 1.15
+
+require github.com/gofiber/fiber v1.14.6
---
+++ sqlite3/sqlite3.go
@@ -0,0 +1,41 @@
+package sqlite3
+
+import (
+	"time"
+)
+
+// Storage interface that is implemented by storage providers
+type Storage struct {
+}
+
+// Config defines the config for mysql storage.
+type Config struct {
+}
+
+// ConfigDefault is the default config
+var ConfigDefault = Config{}
+
+// New creates a new mysql storage
+func New(config ...Config) Storage {
+	return Storage{}
+}
+
+// Get value by key
+func (store Storage) Get(key string) ([]byte, error) {
+	return []byte{}, nil
+}
+
+// Set key with value
+func (store Storage) Set(key string, val []byte, exp time.Duration) error {
+	return nil
+}
+
+// Delete key by key
+func (store Storage) Delete(key string) error {
+	return nil
+}
+
+// Clear all keys
+func (store Storage) Clear() error {
+	return nil
+}
