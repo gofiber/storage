c14befe194f18537a9bf17f2abc3b59d833eb60d
Fenny <25108519+Fenny@users.noreply.github.com>
2020-10-31 10:16:11 +0100

    🎨 update memory storage

---
+++ memory/config.go
@@ -0,0 +1,21 @@
+package memory
+
+import "time"
+
+// Config defines the config for memory storage.
+type Config struct {
+	GCInterval time.Duration
+}
+
+// ConfigDefault is the default config
+var ConfigDefault = Config{
+	GCInterval: 10 * time.Second,
+}
+
+// Helper function to set default values
+func configDefault(cfg Config) Config {
+	if int(cfg.GCInterval) == 0 {
+		cfg.GCInterval = ConfigDefault.GCInterval
+	}
+	return cfg
+}
