42714289aae420fabbb96ec201cfb82f94650c74
hi019 <65871571+hi019@users.noreply.github.com>
2020-10-31 11:53:04 -0400

    Add tests

--- sqlite3/sqlite3.go
+++ sqlite3/sqlite3.go
@@ -85,7 +85,7 @@ func (s *Storage) Get(key string) ([]byte, error) {
 
 	// If the expiration time has already passed, then return nil
 	if time.Now().After(time.Unix(exp, 0)) {
-		return nil, nil
+		return []byte{}, nil
 	}
 
 	return data, nil
