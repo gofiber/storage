9152ce37fe3ee66ea28d83132ef92a34e2856b08
Fenny <25108519+Fenny@users.noreply.github.com>
2020-10-31 07:55:18 +0100

    📦 update redis

---
+++ redis/config.go
@@ -0,0 +1,106 @@
+package redis
+
+import (
+	"context"
+	"crypto/tls"
+	"net"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Config defines the config for redis storage.
+type Config struct {
+	// The network type, either tcp or unix.
+	// Default is tcp.
+	Network string
+	// host:port address.
+	Addr string
+
+	// Dialer creates new network connection and has priority over
+	// Network and Addr options.
+	Dialer func(ctx context.Context, network, addr string) (net.Conn, error)
+
+	// Hook that is called when new connection is established.
+	OnConnect func(ctx context.Context, cn *redis.Conn) error
+
+	// Use the specified Username to authenticate the current connection
+	// with one of the connections defined in the ACL list when connecting
+	// to a Redis 6.0 instance, or greater, that is using the Redis ACL system.
+	Username string
+	// Optional password. Must match the password specified in the
+	// requirepass server configuration option (if connecting to a Redis 5.0 instance, or lower),
+	// or the User Password when connecting to a Redis 6.0 instance, or greater,
+	// that is using the Redis ACL system.
+	Password string
+
+	// Database to be selected after connecting to the server.
+	DB int
+
+	// Maximum number of retries before giving up.
+	// Default is 3 retries.
+	MaxRetries int
+	// Minimum backoff between each retry.
+	// Default is 8 milliseconds; -1 disables backoff.
+	MinRetryBackoff time.Duration
+	// Maximum backoff between each retry.
+	// Default is 512 milliseconds; -1 disables backoff.
+	MaxRetryBackoff time.Duration
+
+	// Dial timeout for establishing new connections.
+	// Default is 5 seconds.
+	DialTimeout time.Duration
+	// Timeout for socket reads. If reached, commands will fail
+	// with a timeout instead of blocking. Use value -1 for no timeout and 0 for default.
+	// Default is 3 seconds.
+	ReadTimeout time.Duration
+	// Timeout for socket writes. If reached, commands will fail
+	// with a timeout instead of blocking.
+	// Default is ReadTimeout.
+	WriteTimeout time.Duration
+
+	// Maximum number of socket connections.
+	// Default is 10 connections per every CPU as reported by runtime.NumCPU.
+	PoolSize int
+	// Minimum number of idle connections which is useful when establishing
+	// new connection is slow.
+	MinIdleConns int
+	// Connection age at which client retires (closes) the connection.
+	// Default is to not close aged connections.
+	MaxConnAge time.Duration
+	// Amount of time client waits for connection if all connections
+	// are busy before returning an error.
+	// Default is ReadTimeout + 1 second.
+	PoolTimeout time.Duration
+	// Amount of time after which client closes idle connections.
+	// Should be less than server's timeout.
+	// Default is 5 minutes. -1 disables idle timeout check.
+	IdleTimeout time.Duration
+	// Frequency of idle checks made by idle connections reaper.
+	// Default is 1 minute. -1 disables idle connections reaper,
+	// but idle connections are still discarded by the client
+	// if IdleTimeout is set.
+	IdleCheckFrequency time.Duration
+
+	// Enables read only queries on slave nodes.
+	readOnly bool
+
+	// TLS Config to use. When set TLS will be negotiated.
+	TLSConfig *tls.Config
+
+	// Limiter interface used to implemented circuit breaker or rate limiter.
+	Limiter redis.Limiter
+}
+
+// ConfigDefault is the default config
+var ConfigDefault = Config{
+	Addr: "127.0.0.1:6379",
+}
+
+// Helper function to set default values
+func configDefault(cfg Config) Config {
+	if cfg.Addr == "" {
+		cfg.Addr = ConfigDefault.Addr
+	}
+	return cfg
+}
--- redis/redis.go
+++ redis/redis.go
@@ -1,41 +1,85 @@
 package redis
 
 import (
+	"context"
 	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 // Storage interface that is implemented by storage providers
 type Storage struct {
+	cfg Config
+	db  *redis.Client
 }
-
-// Config defines the config for redis storage.
-type Config struct {
-}
-
-// ConfigDefault is the default config
-var ConfigDefault = Config{}
 
 // New creates a new redis storage
 func New(config ...Config) Storage {
-	return Storage{}
+	// Set default config
+	cfg := ConfigDefault
+
+	// Override config if provided
+	if len(config) > 0 {
+		cfg = configDefault(config[0])
+
+	}
+
+	// Create new redis client
+	db := redis.NewClient(&redis.Options{
+		Network:            cfg.Network,
+		Addr:               cfg.Addr,
+		Password:           cfg.Password,
+		DB:                 cfg.DB,
+		MaxRetries:         cfg.MaxRetries,
+		MinRetryBackoff:    cfg.MinRetryBackoff,
+		MaxRetryBackoff:    cfg.MaxRetryBackoff,
+		DialTimeout:        cfg.DialTimeout,
+		ReadTimeout:        cfg.ReadTimeout,
+		WriteTimeout:       cfg.WriteTimeout,
+		PoolSize:           cfg.PoolSize,
+		MinIdleConns:       cfg.MinIdleConns,
+		MaxConnAge:         cfg.MaxConnAge,
+		PoolTimeout:        cfg.PoolTimeout,
+		IdleTimeout:        cfg.IdleTimeout,
+		IdleCheckFrequency: cfg.IdleCheckFrequency,
+		TLSConfig:          cfg.TLSConfig,
+		Limiter:            cfg.Limiter,
+	})
+
+	// Test connection
+	if err := db.Ping(context.Background()).Err(); err != nil {
+		panic(err)
+	}
+	// Create new store
+	return Storage{
+		cfg: cfg,
+		db:  db,
+	}
 }
 
 // Get value by key
 func (store Storage) Get(key string) ([]byte, error) {
-	return []byte{}, nil
+	val, err := store.db.Get(context.Background(), key).Bytes()
+	if err != nil {
+		if err != redis.Nil {
+			return nil, err
+		}
+		return nil, nil
+	}
+	return val, nil
 }
 
 // Set key with value
 func (store Storage) Set(key string, val []byte, exp time.Duration) error {
-	return nil
+	return store.db.Set(context.Background(), key, val, exp).Err()
 }
 
 // Delete key by key
 func (store Storage) Delete(key string) error {
-	return nil
+	return store.db.Del(context.Background(), key).Err()
 }
 
 // Clear all keys
 func (store Storage) Clear() error {
-	return nil
+	return store.db.FlushDB(context.Background()).Err()
 }
