a5b87a0d48812545542a0bf03a323ee973666656
Emre Savcı <emre.savci@trendyol.com>
2023-05-29 11:41:04 +0300

    add couchbase storage

---
+++ couchbase/couchbase_test.go
@@ -0,0 +1,135 @@
+package couchbase
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gofiber/utils"
+)
+
+func TestSetCouchbase_ShouldReturnNoError(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Set("test", []byte("test"), 0)
+
+	utils.AssertEqual(t, nil, err)
+}
+
+func TestGetCouchbase_ShouldReturnNil_WhenDocumentNotFound(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	val, err := testStorage.Get("not_found_key")
+
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 0, len(val))
+}
+
+func TestSetAndGet_GetShouldReturn_SettedValueWithoutError(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Set("test", []byte("fiber_test_value"), 0)
+	utils.AssertEqual(t, nil, err)
+
+	val, err := testStorage.Get("test")
+
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, val, []byte("fiber_test_value"))
+}
+
+func TestSetAndGet_GetShouldReturnNil_WhenTTLExpired(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Set("test", []byte("fiber_test_value"), 3*time.Second)
+	utils.AssertEqual(t, nil, err)
+
+	time.Sleep(6 * time.Second)
+
+	val, err := testStorage.Get("test")
+
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 0, len(val))
+}
+
+func TestSetAndDelete_DeleteShouldReturn_NoError(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Set("test", []byte("fiber_test_value"), 0)
+	utils.AssertEqual(t, nil, err)
+
+	err = testStorage.Delete("test")
+	utils.AssertEqual(t, nil, err)
+
+	_, err = testStorage.Get("test")
+	errStr := err.Error()
+
+	utils.AssertEqual(t, true, strings.Contains(errStr, "document not found"))
+}
+
+func TestSetAndReset_ResetShouldReturn_NoError(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Set("test", []byte("fiber_test_value"), 0)
+	utils.AssertEqual(t, nil, err)
+
+	err = testStorage.Reset()
+	utils.AssertEqual(t, nil, err)
+
+	_, err = testStorage.Get("test")
+	errStr := err.Error()
+
+	utils.AssertEqual(t, true, strings.Contains(errStr, "document not found"))
+}
+
+func TestClose_CloseShouldReturn_NoError(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+
+	err := testStorage.Close()
+	utils.AssertEqual(t, nil, err)
+}
+
+func TestGetConn_ReturnsNotNill(t *testing.T) {
+	testStorage := New(Config{
+		Username: "admin",
+		Password: "123456",
+		Host:     "127.0.0.1:8091",
+		Bucket:   "fiber_storage",
+	})
+	conn := testStorage.Conn()
+	utils.AssertEqual(t, true, conn != nil)
+}
---
+++ couchbase/go.mod
@@ -0,0 +1,12 @@
+module github.com/gofiber/storage/couchbase
+
+go 1.18
+
+require github.com/gofiber/utils v1.1.0
+
+require (
+	github.com/couchbase/gocb/v2 v2.6.3 // indirect
+	github.com/couchbase/gocbcore/v10 v10.2.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
