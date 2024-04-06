c93cef290e379b3a0ba959ac3484405928bab2ac
xEricL <ericlatawiec123@gmail.com>
2024-04-06 14:05:41 -0400

    implement DeleteMany()

--- s3/s3_methods_test.go
+++ s3/s3_methods_test.go
@@ -18,6 +18,34 @@ func Test_S3_CreateDeleteBucket(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func Test_S3_DeleteMany(t *testing.T) {
+	val := []byte("doe")
+
+	err := testStore.Set("john1", val, 0)
+	require.NoError(t, err)
+
+	err = testStore.Set("john2", val, 0)
+	require.NoError(t, err)
+
+	err = testStore.Set("john3", val, 0)
+	require.NoError(t, err)
+
+	err = testStore.DeleteMany("john1", "john2")
+	require.NoError(t, err)
+
+	result, err := testStore.Get("john1")
+	require.NoError(t, err)
+	require.Zero(t, len(result))
+
+	result, err = testStore.Get("john2")
+	require.NoError(t, err)
+	require.Zero(t, len(result))
+
+	result, err = testStore.Get("john3")
+	require.NoError(t, err)
+	require.Equal(t, val, result)
+}
+
 func Test_S3_SetWithChecksum(t *testing.T) {
 	var (
 		key = "set-with-checksum"
