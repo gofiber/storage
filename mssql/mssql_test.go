4c2246e2b97b9618cfc175a03574566360a6ca00
Manuel de la Peña <mdelapenya@gmail.com>
2025-01-30 17:28:01 +0100

    Revert "feat: use testcontainers-go's MSSQL module"
    This reverts commit 6742f2ae27f2af204d3a801272ab6d0ed5829965.

--- mssql/mssql_test.go
+++ mssql/mssql_test.go
@@ -1,53 +1,20 @@
 package mssql
 
 import (
-	"context"
 	"database/sql"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go/modules/mssql"
 )
 
-const (
-	// mssqlImage is the default image used for running MSSQL in tests.
-	mssqlImage              = "mcr.microsoft.com/mssql/server:2022-RTM-GDR1-ubuntu-20.04"
-	mssqlImageEnvVar string = "TEST_MSSQL_IMAGE"
-	mssqlUser        string = "sa"
-	mssqlPass        string = "MsSql!1234"
-	mssqlDatabase    string = "master"
-)
-
-func newTestStore(t testing.TB) (*Storage, error) {
-	t.Helper()
-
-	img := mssqlImage
-	if imgFromEnv := os.Getenv(mssqlImageEnvVar); imgFromEnv != "" {
-		img = imgFromEnv
-	}
-
-	ctx := context.Background()
-	// Due to licensing restrictions you are required to explicitly accept an EULA for this container image.
-	c, err := mssql.Run(ctx, img,
-		mssql.WithPassword(mssqlPass),
-		mssql.WithAcceptEULA(),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	conn, err := c.ConnectionString(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return New(Config{
-		ConnectionURI: conn,
-		Reset:         true,
-	}), nil
-}
+var testStore = New(Config{
+	Database: os.Getenv("MSSQL_DATABASE"),
+	Username: os.Getenv("MSSQL_USERNAME"),
+	Password: os.Getenv("MSSQL_PASSWORD"),
+	Reset:    true,
+})
 
 func Test_MSSQL_Set(t *testing.T) {
 	var (
@@ -55,10 +22,7 @@ func Test_MSSQL_Set(t *testing.T) {
 		val = []byte("doe")
 	)
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set(key, val, 0)
+	err := testStore.Set(key, val, 0)
 	require.NoError(t, err)
 }
 
@@ -68,10 +32,7 @@ func Test_MSSQL_Set_Override(t *testing.T) {
 		val = []byte("doe")
 	)
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set(key, val, 0)
+	err := testStore.Set(key, val, 0)
 	require.NoError(t, err)
 
 	err = testStore.Set(key, val, 0)
@@ -84,10 +45,7 @@ func Test_MSSQL_Get(t *testing.T) {
 		val = []byte("doe")
 	)
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set(key, val, 0)
+	err := testStore.Set(key, val, 0)
 	require.NoError(t, err)
 
 	result, err := testStore.Get(key)
@@ -102,10 +60,7 @@ func Test_MSSQL_Set_Expiration(t *testing.T) {
 		exp = 1 * time.Second
 	)
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set(key, val, exp)
+	err := testStore.Set(key, val, exp)
 	require.NoError(t, err)
 
 	time.Sleep(1100 * time.Millisecond)
@@ -114,18 +69,12 @@ func Test_MSSQL_Set_Expiration(t *testing.T) {
 func Test_MSSQL_Get_Expired(t *testing.T) {
 	key := "john"
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
 	result, err := testStore.Get(key)
 	require.NoError(t, err)
 	require.Zero(t, len(result))
 }
 
 func Test_MSSQL_Get_NotExist(t *testing.T) {
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
 	result, err := testStore.Get("notexist")
 	require.NoError(t, err)
 	require.Zero(t, len(result))
@@ -137,10 +86,7 @@ func Test_MSSQL_Delete(t *testing.T) {
 		val = []byte("doe")
 	)
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set(key, val, 0)
+	err := testStore.Set(key, val, 0)
 	require.NoError(t, err)
 
 	err = testStore.Delete(key)
@@ -154,10 +100,7 @@ func Test_MSSQL_Delete(t *testing.T) {
 func Test_MSSQL_Reset(t *testing.T) {
 	val := []byte("doe")
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set("john1", val, 0)
+	err := testStore.Set("john1", val, 0)
 	require.NoError(t, err)
 
 	err = testStore.Set("john2", val, 0)
@@ -178,11 +121,8 @@ func Test_MSSQL_Reset(t *testing.T) {
 func Test_MSSQL_GC(t *testing.T) {
 	testVal := []byte("doe")
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
 	// This key should expire
-	err = testStore.Set("john", testVal, time.Nanosecond)
+	err := testStore.Set("john", testVal, time.Nanosecond)
 	require.NoError(t, err)
 
 	testStore.gc(time.Now())
@@ -203,10 +143,7 @@ func Test_MSSQL_GC(t *testing.T) {
 func Test_MSSQL_Non_UTF8(t *testing.T) {
 	val := []byte("0xF5")
 
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
-	err = testStore.Set("0xF6", val, 0)
+	err := testStore.Set("0xF6", val, 0)
 	require.NoError(t, err)
 
 	result, err := testStore.Get("0xF6")
@@ -230,26 +167,18 @@ func Test_SslRequiredMode(t *testing.T) {
 }
 
 func Test_MSSQL_Close(t *testing.T) {
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
 	require.Nil(t, testStore.Close())
 }
 
 func Test_MSSQL_Conn(t *testing.T) {
-	testStore, err := newTestStore(t)
-	require.NoError(t, err)
-
 	require.True(t, testStore.Conn() != nil)
 }
 
 func Benchmark_MSSQL_Set(b *testing.B) {
-	testStore, err := newTestStore(b)
-	require.NoError(b, err)
-
 	b.ReportAllocs()
 	b.ResetTimer()
 
+	var err error
 	for i := 0; i < b.N; i++ {
 		err = testStore.Set("john", []byte("doe"), 0)
 	}
@@ -258,10 +187,7 @@ func Benchmark_MSSQL_Set(b *testing.B) {
 }
 
 func Benchmark_MSSQL_Get(b *testing.B) {
-	testStore, err := newTestStore(b)
-	require.NoError(b, err)
-
-	err = testStore.Set("john", []byte("doe"), 0)
+	err := testStore.Set("john", []byte("doe"), 0)
 	require.NoError(b, err)
 
 	b.ReportAllocs()
@@ -275,12 +201,10 @@ func Benchmark_MSSQL_Get(b *testing.B) {
 }
 
 func Benchmark_MSSQL_SetAndDelete(b *testing.B) {
-	testStore, err := newTestStore(b)
-	require.NoError(b, err)
-
 	b.ReportAllocs()
 	b.ResetTimer()
 
+	var err error
 	for i := 0; i < b.N; i++ {
 		_ = testStore.Set("john", []byte("doe"), 0)
 		err = testStore.Delete("john")
