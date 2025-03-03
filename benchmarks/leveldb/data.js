e9aede0cfa8e2e6841b221cc327d5faec442ade9
github-action-benchmark <github@users.noreply.github.com>
2025-03-03 07:26:41 +0000

    add Benchmark (go) benchmark result for 6b31862a336b1e1adfbecb41ced9e2f50aaafe53

---
+++ benchmarks/leveldb/data.js
@@ -0,0 +1,104 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740986800933,
+  "repoUrl": "https://github.com/gofiber/storage",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rene@gofiber.io",
+            "name": "RW",
+            "username": "ReneWerner87"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b31862a336b1e1adfbecb41ced9e2f50aaafe53",
+          "message": "Merge pull request #1565 from SadikSunbul/main\n\n🔥 feat: Add LevelDB driver",
+          "timestamp": "2025-03-03T08:25:27+01:00",
+          "tree_id": "4315f3b0a376032589ecea0ba3ea9ce6e04341ba",
+          "url": "https://github.com/gofiber/storage/commit/6b31862a336b1e1adfbecb41ced9e2f50aaafe53"
+        },
+        "date": 1740986793162,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Set",
+            "value": 2681,
+            "unit": "ns/op\t     303 B/op\t       2 allocs/op",
+            "extra": "474196 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Set - ns/op",
+            "value": 2681,
+            "unit": "ns/op",
+            "extra": "474196 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Set - B/op",
+            "value": 303,
+            "unit": "B/op",
+            "extra": "474196 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Set - allocs/op",
+            "value": 2,
+            "unit": "allocs/op",
+            "extra": "474196 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Get",
+            "value": 467.7,
+            "unit": "ns/op\t     357 B/op\t      10 allocs/op",
+            "extra": "2718206 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Get - ns/op",
+            "value": 467.7,
+            "unit": "ns/op",
+            "extra": "2718206 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Get - B/op",
+            "value": 357,
+            "unit": "B/op",
+            "extra": "2718206 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Get - allocs/op",
+            "value": 10,
+            "unit": "allocs/op",
+            "extra": "2718206 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Delete",
+            "value": 2760,
+            "unit": "ns/op\t     306 B/op\t       3 allocs/op",
+            "extra": "469500 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Delete - ns/op",
+            "value": 2760,
+            "unit": "ns/op",
+            "extra": "469500 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Delete - B/op",
+            "value": 306,
+            "unit": "B/op",
+            "extra": "469500 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Delete - allocs/op",
+            "value": 3,
+            "unit": "allocs/op",
+            "extra": "469500 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
