1c79e4b1986d3d10aeb96e019571f75bb2e670a9
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 08:24:46 +0000

    add Benchmark (go) benchmark result for b32cc8185e4501119e31a0d9686ed8dc941ea429

--- benchmarks/memcache/data.js
+++ benchmarks/memcache/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752653194467,
+  "lastUpdate": 1762417486335,
   "repoUrl": "https://github.com/gofiber/storage",
   "entries": {
     "Benchmark": [
@@ -384,6 +384,102 @@ window.BENCHMARK_DATA = {
             "value": 4,
             "unit": "allocs/op",
             "extra": "5324 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rene@gofiber.io",
+            "name": "RW",
+            "username": "ReneWerner87"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b32cc8185e4501119e31a0d9686ed8dc941ea429",
+          "message": "Merge pull request #1970 from gofiber/dependabot/go_modules/memcache/github.com/docker/docker-28.3.3incompatible\n\nchore(deps): bump github.com/docker/docker from 28.2.2+incompatible to 28.3.3+incompatible in /memcache",
+          "timestamp": "2025-11-06T09:00:42+01:00",
+          "tree_id": "aa672588794a7e4f72af3579b49955c5c35a89a1",
+          "url": "https://github.com/gofiber/storage/commit/b32cc8185e4501119e31a0d9686ed8dc941ea429"
+        },
+        "date": 1762417482428,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Memcache_Set",
+            "value": 132435,
+            "unit": "ns/op\t      35 B/op\t       3 allocs/op",
+            "extra": "9843 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Set - ns/op",
+            "value": 132435,
+            "unit": "ns/op",
+            "extra": "9843 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Set - B/op",
+            "value": 35,
+            "unit": "B/op",
+            "extra": "9843 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Set - allocs/op",
+            "value": 3,
+            "unit": "allocs/op",
+            "extra": "9843 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Get",
+            "value": 123472,
+            "unit": "ns/op\t      96 B/op\t       4 allocs/op",
+            "extra": "8934 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Get - ns/op",
+            "value": 123472,
+            "unit": "ns/op",
+            "extra": "8934 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Get - B/op",
+            "value": 96,
+            "unit": "B/op",
+            "extra": "8934 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_Get - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "8934 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_SetAndDelete",
+            "value": 247276,
+            "unit": "ns/op\t      51 B/op\t       4 allocs/op",
+            "extra": "4707 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_SetAndDelete - ns/op",
+            "value": 247276,
+            "unit": "ns/op",
+            "extra": "4707 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_SetAndDelete - B/op",
+            "value": 51,
+            "unit": "B/op",
+            "extra": "4707 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_Memcache_SetAndDelete - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "4707 times\n4 procs"
           }
         ]
       }
