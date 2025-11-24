62319e51b4da2d1435aa266445bff2fad16833c6
Manuel de la Peña <mdelapenya@gmail.com>
2025-11-24 17:02:27 +0100

    fix(mysql): use surrealdb pattern

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -129,10 +129,7 @@ func (s *Storage) GetWithContext(ctx context.Context, key string) ([]byte, error
 
 	// If the expiration time has already passed, then return nil
 	if exp != 0 && exp <= time.Now().Unix() {
-		_, err := s.db.ExecContext(ctx, s.sqlDelete, key)
-		if err != nil {
-			return nil, fmt.Errorf("delete expired key: %w", err)
-		}
+		_, _ = s.db.ExecContext(ctx, s.sqlDelete, key)
 		return nil, nil
 	}
 
